61c2ca7b9a6579cc6eaad3e7ab143d3c12974b18
Arul-1911 <arulpraveen19112002@gmail.com>
2023-11-07 14:54:59 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Pricing from './Example';
 
 function App() {
+
+  let plandetails = [{
+title : "Free",
+amount : 0,
+user : "Single User",
+storage : "5gb Storage",
+public_projects :"Unlimited public projects",
+access : "Community access" ,
+sub_domain : "Free Sub-Domain",
+status : " Monthly Status Reports",
+paid : true,
+amt : true
+  },
+{
+ title : "paid",
+amount : 15,
+user : "Three Users",
+storage : "25gb Storage",
+public_projects :"Unlimited public projects",
+access : "Community access" ,
+sub_domain : "Free Sub-Domain",
+status : " Monthly Status Reports",
+paid : false,
+amt : true
+
+},
+{
+  title : "Premium",
+  amount : 100,
+  user : "Multiple Users",
+  storage : "150gb Storage",
+  public_projects :"Unlimited public projects",
+  access : "Community access" ,
+  sub_domain : "Free Sub-Domain",
+  status : " Monthly Status Reports",
+  paid : false,
+  amt: false
+}]
   return (
-    <div className="App">
-      <Pricing/>
-    </div>
+    <div className="container_1">
+    <section className="pricing">
+      <div className="container">
+        <div className="row">
+    {
+      plandetails.map((detail) => {
+        return <Pricing details={detail}></Pricing>
+      })
+    }
+ 
+        </div>
+      </div>
+    </section>
+     </div>
   );
 }
 
--- src/Example.jsx
+++ src/Example.jsx
@@ -4,192 +4,63 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faXmark} from '@fortawesome/free-solid-svg-icons';
 
 
-const Pricing = () => {
+const Pricing = ({details}) => {
   return (
-    <div className="container_1">
-    <section className="pricing">
-      <div className="container">
-        <div className="row">
-            
+           <> 
           {/* Free card 1 */}
+          
           <div className="col-lg-4 col-md-9 pricing-card">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title text-muted text-uppercase text-center">Free</h5>
-                <h6 className="card-price text-center">$0<span className="period">/month</span></h6>
+                <h5 className="card-title text-muted text-uppercase text-center">{details.title}</h5>
+                <h6 className="card-price text-center">$<span className="period">{details.amount}/month</span></h6>
                 <hr />
                 <ul className="fa-ul">
                   <li>
                     <span className="fa-li">
                       <FontAwesomeIcon icon={faCheck} />
                     </span>
-                    Single User
+                   {details.user}
                   </li>
                   <li>
                     <span className="fa-li">
                       <FontAwesomeIcon icon={faCheck} />
                     </span>
-                    5GB Storage
+                    {details.storage}
                   </li>
                   <li>
                     <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
+                    {details.amt ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faCheck} />}
                     </span>
-                    Demo
+                    {details.public_projects}
                   </li>
                   <li>
                     <span className="fa-li">
-                      <FontAwesomeIcon icon={faXmark} />
+                    {details.paid ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faCheck} />}
                     </span>
-                    Unlimited Projects
+                    {details.access}
                   </li>
                   <li>
                     <span className="fa-li">
-                      <FontAwesomeIcon icon={faXmark} />
+                    {details.amt ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faCheck} />}
                     </span>
-                    Phone Support
+                    {details.sub_domain}
                   </li>
                   <li>
                     <span className="fa-li">
-                      <FontAwesomeIcon icon={faXmark} />
+                    {details.paid ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faCheck} />}
                     </span>
-                    Free Sub-Domain
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faXmark} />
-                    </span>
-                   Monthly Status Reports
+                    {details.status}
                   </li>
                 </ul>
                 <div className="d-grid">
-                  <button  className="btn btn-primary text-uppercase">Button</button>
+                  <button  className="btn btn-primary text-uppercase">Buy</button>
                 </div>
               </div>
             </div>
           </div>
 
-
-          {/* Plus card 2 */}
-          <div className="col-lg-4 col-md-9  pricing-card">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title text-muted text-uppercase text-center">Plus</h5>
-                <h6 className="card-price text-center">$9<span className="period">/month</span></h6>
-                <hr />
-                <ul className="fa-ul">
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    Single User
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    5GB Storage
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    Demo
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    Unlimited Projects
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    Phone Support
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    Free Sub-Domain
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faXmark} />
-                    </span>
-                   Monthly Status Reports
-                  </li>
-                </ul>
-                <div className="d-grid">
-                  <button  className="btn btn-primary text-uppercase">Button</button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-          {/* Pro card  3*/}
-          <div className="col-lg-4 col-md-9  pricing-card">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title text-muted text-uppercase text-center">Pro</h5>
-                <h6 className="card-price text-center">$49<span className="period">/month</span></h6>
-                <hr />
-                <ul className="fa-ul">
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    Single User
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    5GB Storage
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    Demo
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    Unlimited Projects
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    Phone Support
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    Free Sub-Domain
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                   Monthly Status Reports
-                  </li>
-                </ul>
-                <div className="d-grid">
-                  <button  className="btn btn-primary text-uppercase">Button</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-     </div>
+          </>   
   );
 };
 
